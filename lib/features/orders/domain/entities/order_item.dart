6aad95df52a3db15b38edc0d179516faf6479f2f
ahel-men <ahel-men@student.1337.ma>
2022-01-05 22:20:38 +0100

    added orderItem class entitie on domain layer

---
+++ lib/features/orders/domain/entities/order_item.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+class OrderItem extends Equatable {
+  final String object;
+  final int id;
+  final String name;
+  final double price;
+  final String currency;
+  final String color;
+
+  OrderItem(
+      {required this.object,
+      required this.id,
+      required this.name,
+      required this.price,
+      required this.currency,
+      required this.color});
+
+  @override
+  List<Object> get props => [object, id, name, price, currency, color];
+}
